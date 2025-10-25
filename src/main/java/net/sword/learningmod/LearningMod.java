f00ea03da1c1842e5c85f84c836c5d2d8d32218b
sword <ryswordan@gmail.com>
2025-10-25 20:32:06 +0800

    Reshaped Project Structure

--- src/main/java/net/sword/learningmod/LearningMod.java
+++ src/main/java/net/sword/learningmod/LearningMod.java
@@ -1,5 +1,6 @@
 package net.sword.learningmod;
 
+import net.sword.learningmod.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -50,6 +51,7 @@ public class LearningMod {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -64,6 +66,10 @@ public class LearningMod {
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
 
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.BISMUTH);
+            event.accept(ModItems.RAW_BISMUTH);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
